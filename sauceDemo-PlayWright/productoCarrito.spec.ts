3e6f63fa6c4db590321e5b75d95f917b8a875e94
Sbarraco <santonedaniel.2@gmail.com>
2026-05-27 22:00:10 -0300

    Add files via upload

---
+++ sauceDemo-PlayWright/productoCarrito.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+
+test('standart_User', async ({page}) =>{
+
+    await  page.goto ('https://www.saucedemo.com/')
+
+    await page.getByRole('textbox', {name:'Username'}).fill('standard_user')
+    await page.getByRole('textbox', {name:'Password'}).fill('secret_sauce')
+    await page.getByRole('button', {name:'Login'}).click()
+                
+    const item = page.locator('#inventory_container .inventory_item').filter({
+  hasText: 'Sauce Labs Backpack'})
+
+await item.getByRole('button', { name: 'Add to cart' }).click()
+    await page.pause()
+
+});
